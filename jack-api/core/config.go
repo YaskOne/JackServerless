cdd1b4866efe61168b383e32fa96ad6052830212
Ngo Van Arthur <arthur.ngovan@gmail.com>
2019-08-23 15:31:28 +0200

    Update config.go

--- jack-api/core/config.go
+++ jack-api/core/config.go
@@ -1,13 +1,11 @@
 package core
 
-var DbUser = "JackAdmin"
-var DbPassword = "ArgosBubble3"
+var DbUser = ""
+var DbPassword = ""
 
-//var DbName = "JackDB"
-var DbName = "jackdevdb"
+var DbName = ""
 //
-//var Endpoint = "jackdbmysql.crqo2vw40anm.eu-west-2.rds.amazonaws.com:3308"
-var Endpoint = "jackdevdbmysql.crqo2vw40anm.eu-west-2.rds.amazonaws.com:3306"
+var Endpoint = ""
 
 //var Develop = false
 var Develop = true
